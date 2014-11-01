dfd19efb811e04dc4ae46893dff55fd9ea67ed40
sin <sin@2f30.org>
2014-11-01 15:20:11 +0000

    default to 2 pixels

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ char *colors[] = {
 	[COLOR_BATT_LEFT2DRAIN]  = "blue"
 };
 
-unsigned int thickness = 4;	/* 4 pixels by default */
+unsigned int thickness = 2;	/* 2 pixels by default */
 time_t pollinterval = 5;	/* poll battery state every 5 seconds */
 int bottom = 1;			/* set to 0 if you want the bar to be at the top */
 int maxcap = 100;		/* maximum battery capacity */
